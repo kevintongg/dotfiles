11e7c174eb7c038f56a8d6ca1840a28e5a319cad
Kevin Tong <kcr1230@hotmail.com>
2017-05-29 17:36:28 -0700

    Add files via upload

---
+++ hyper.js
@@ -0,0 +1,80 @@
+module.exports = {
+  config: {
+    // default font size in pixels for all tabs
+    fontSize: 14,
+
+    // font family with optional fallbacks
+    fontFamily: 'Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
+
+    // terminal cursor background color (hex)
+    cursorColor: '#999999',
+
+    // color of the text
+    foregroundColor: '#fff',
+
+    // terminal background color
+    backgroundColor: '#000',
+
+    // border color (window, tabs)
+    borderColor: '#333',
+
+    // custom css to embed in the main window
+    css: '',
+
+    // custom padding (css format, i.e.: `top right bottom left`)
+    termCSS: '',
+
+    // custom padding
+    padding: '12px 14px',
+
+    // some color overrides. see http://bit.ly/29k1iU2 for
+    // the full list
+    colors: {
+      black: '#000000',
+      red: '#ff0000',
+      green: '#33ff00',
+      yellow: '#ffff00',
+      blue: '#0066ff',
+      magenta: '#cc00ff',
+      cyan: '#00ffff',
+      white: '#d0d0d0',
+      lightBlack: '#808080',
+      lightRed: '#ff0000',
+      lightGreen: '#33ff00',
+      lightYellow: '#ffff00',
+      lightBlue: '#0066ff',
+      lightMagenta: '#cc00ff',
+      lightCyan: '#00ffff',
+      lightWhite: '#fff'
+    },
+  },
+
+  // a list of plugins to fetch and install from npm
+  // format: [@org/]project[#version]
+  // examples:
+  //   `hypersolar`
+  //   `@company/project`
+  //   `project#1.0.1`
+  plugins: [
+    'hyper-blink',
+    'hypercwd',
+    'hyperlinks',
+    'hyperterm-bold-tab',
+    'hyperterm-tab-icons',
+    'hyper-smyck',
+    'htyt',
+    'hyperline',
+    'hyperterm-tabs',
+    'hyperterm-dibdabs',
+    'hyper-transparent-dynamic',
+    'hyperterm-paste',
+    'hyperterm-alternatescroll',
+    'hyperterm-cursor',
+    "hypertheme"
+  ],
+
+  // in development, you can create a directory under
+  // `~/.hyperterm_plugins/local/` and include it here
+  // to load it and avoid it being `npm install`ed
+  localPlugins: []
+};
